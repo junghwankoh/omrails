f09ea89748757ba62a01396f0503f3e1c4dc2cd8
Michael Koh <junghwan.koh@gmail.com>
2014-09-24 22:20:48 +1200

    omrails on rails version 3.2.19

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newomrails::Application.config.secret_token = 'ef2fca6097109c76b166c2144ea5a9613d15da0812afc863c3b844345d8dbb0b55cae45894c19bc92ae4d91ea79de1528acc3c429751ac82921ee2ec88897241'
