1b4f42b6efbf095fb4f2623df2300debd8caf603
Michael Koh <junghwan.koh@gmail.com>
2014-11-10 21:37:53 +1300

    ran the function *rails generate paperclip pin image* to fix error on heroku as according to *heroku logs --tail*

--- db/migrate/20141004095012_add_attachment_image_to_pins.rb
+++ db/migrate/20141004095012_add_attachment_image_to_pins.rb
@@ -2,18 +2,10 @@ class AddAttachmentImageToPins < ActiveRecord::Migration
   def self.up
     change_table :pins do |t|
       t.attachment :image
-    add_column :pins, :image_file_name, :string
-    add_column :pins, :image_content_type, :string
-    add_column :pins, :image_file_size, :integer
-    add_column :pins, :image_updated_at, :datetime
     end
   end
 
   def self.down
     remove_attachment :pins, :image
-    remove_column :pins, :image_file_name
-    remove_column :pins, :image_content_type
-    remove_column :pins, :image_file_size
-    remove_column :pins, :image_updated_at
   end
 end
